a0fcd5991f9d02a9fd0e71db53a00582de492a5e
becky <jromi158@gmail.com>
2020-05-24 23:38:30 +0900

    add google login button

--- src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
+++ src/main/java/com/jojoldu/book/springboot/domain/user/Role.java
@@ -1,5 +1,6 @@
 package com.jojoldu.book.springboot.domain.user;
 
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -7,8 +8,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Role {
 
-    GUEST("ROLE_GUEST", "손님");
-//    USER("ROLE_USER", "일반 사용자");
+    GUEST("ROLE_GUEST", "손님"),
+    USER("ROLE_USER", "일반 사용자");
 
     private final String key;
     private final String title;
